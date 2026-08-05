426b80c7ac6ba1e71a64a76fa4f21d3187ed5179
Musahaf Ali <musahafali43@gmail.com>
2026-08-05 12:14:54 +0500

    I have worked now on searchbar

--- jobportal/src/Components/SearchBox.jsx
+++ jobportal/src/Components/SearchBox.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-
+import { IoSearch } from "react-icons/io5";
+import { CiLocationOn } from "react-icons/ci";
 const SearchBox = () => {
   return (
-   <div className=' flex justify-center '>
-    <div className=' rounded-full bg-white mt-5 w-3/4 border border-gray-300  px-5 py-3 flex justify-center  gap-5 items-center'>
-      <input type="text" placeholder='Search job title or Company' className='w-170 bg-white rounded-md py-4 px-4 focus:outline-none '/>
-      <input type="text" placeholder='Location' className='w-100 bg-white rounded-md py-4 px-4 focus:outline-none '/>
-      <button className='bg-blue-500 text-white py-4 px-6 rounded-full hover:bg-blue-600 focus:outline-none '>Search</button>
-    </div>
+   <div className="w-full flex justify-center px-4 mt-5">
+  <div className="w-full mt-3  items-center max-w-4xl bg-white border border-gray-300 rounded-md p-3 flex justify-between flex-col md:flex-row gap-4 ">
+  <div className='flex items-center flex-1 px-3 py-1 rounded-full focus-within:ring-2 focus-within:ring-blue-300'>
+   <IoSearch className='text-gray-500 text-xl'/>
+    <input
+      type="text"
+      placeholder="Search job title or Company"
+      className="w-full md:w-64 lg:w-80 md:flex-1 py-4 px-4 rounded-md outline-none"
+    />
+  </div>
+    <div className="h-10 w-px bg-gray-300"></div>
+   <div className='flex items-center flex-1 px-3 py-1 rounded-full focus-within:ring-2 focus-within:ring-blue-300'>
+     <CiLocationOn className='text-gray-500 text-xl'/>
+    <input
+      type="text"
+      placeholder="Location"
+      className="w-full md:w-64 lg:w-80 py-4 px-4 rounded-md outline-none"
+    />
    </div>
+
+    <button className="w-full md:w-auto bg-blue-500 text-white py-5 px-10 rounded-full hover:bg-blue-600">
+      Search
+    </button>
+
+  </div>
+  </div>
   );
 }
 
