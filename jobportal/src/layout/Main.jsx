eea97df5d3757194caf72f4a0612662e6390301c
Musahaf Ali <musahafali43@gmail.com>
2026-08-11 12:06:33 +0500

    I have made sidebar functional and now everything working smoothly

--- jobportal/src/layout/Main.jsx
+++ jobportal/src/layout/Main.jsx
@@ -1,32 +1,40 @@
-import React, { useContext } from 'react';
-import { jobsData } from '../data/jobs';
-import { JobsContext } from '../context/JobsContext';
+import React, { useContext } from "react";
+import { jobsData } from "../data/jobs";
+import { JobsContext } from "../context/JobsContext";
 const Main = () => {
-  const {search} = useContext(JobsContext)
+  const { search, typeFilter, category } = useContext(JobsContext);
   const filteredData = jobsData.filter((job) => {
-      return (
-        job.title.toLowerCase().includes(search.toLowerCase()) ||
-        job.company.toLowerCase().includes(search.toLowerCase())
+    const matchesSearch =
+      job.title.toLowerCase().includes(search.toLowerCase()) ||
+      job.company.toLowerCase().includes(search.toLowerCase());
+    const matchesType =
+      typeFilter.length === 0 ||
+      typeFilter.some((filter) =>
+        filter == "Remote" ? job.location === "Remote" : job.type === filter,
       );
-    });
+    const matchesCategory = category === "" || job.category === category;
+
+    return matchesSearch && matchesType && matchesCategory
+  });
   return (
     <a href="#">
-        <div className='flex justify-center flex-wrap gap-4 p-4'>
-      {
-        filteredData.map((job) => (
-          <div key={job.id} className='flex   h-40 w-90 gap-4 bg-white p-4 rounded-md  mb-4'>
+      <div className="flex justify-center flex-wrap gap-4 p-4">
+        {filteredData.map((job) => (
+          <div
+            key={job.id}
+            className="flex   h-40 w-90 gap-4 bg-white p-4 rounded-md  mb-4"
+          >
             <div>
-            <h2 className='text-xl font-semibold'>{job.title}</h2>
-            <span className=''>{job.company}</span>
-            <span className='ml-2 text-gray-600'>{job.location}</span>
+              <h2 className="text-xl font-semibold">{job.title}</h2>
+              <span className="">{job.company}</span>
+              <span className="ml-2 text-gray-600">{job.location}</span>
             </div>
-            <span className='text-gray-600'>{job.type}</span>
+            <span className="text-gray-600">{job.type}</span>
           </div>
-        ))
-      }
-    </div>
+        ))}
+      </div>
     </a>
   );
-}
+};
 
 export default Main;
